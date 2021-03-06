3dfa881a5cddc4bf2573558b107fb95f326be1eb
SirBob01 <kleonardo52@gmail.com>
2021-03-06 19:15:28 +1100

    Implement back-end

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'swipe_feed_page.dart';
+import 'backend_calls.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
