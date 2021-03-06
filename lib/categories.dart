cb61ca8cdbccb63dda6a34df3c75bdfbca449e4e
Kaif Ahsan <kaifahsan420@gmail.com>
2021-03-06 16:16:29 +1100

    Make the bottom nav bar functional

---
+++ lib/categories.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CategoriesPage extends StatefulWidget {
+  @override
+  _CategoriesPageState createState() => _CategoriesPageState();
+}
+
+class _CategoriesPageState extends State<CategoriesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Categories page"),),
+      body: Center(child: Text("Browse activities by categories"),),
+    );
+  }
+}
--- lib/swipe_feed_page.dart
+++ lib/swipe_feed_page.dart
@@ -1,5 +1,6 @@
 import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:tinder_cards/categories.dart';
 import 'cards_section_alignment.dart';
 import 'cards_section_draggable.dart';
 
@@ -11,6 +12,8 @@ class SwipeFeedPage extends StatefulWidget {
 class _SwipeFeedPageState extends State<SwipeFeedPage> {
   bool showAlignmentCards = false;
   int currentPage = 0;
+
+  GlobalKey bottomNavigationKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +36,8 @@ class _SwipeFeedPageState extends State<SwipeFeedPage> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[
-          showAlignmentCards
-              ? CardsSectionAlignment(context)
-              : CardsSectionDraggable(),
-          /*buttonsRow()*/
-        ],
+      body: Container(
+        child: _getPage(currentPage),
       ),
         bottomNavigationBar: FancyBottomNavigation(
           tabs: [
@@ -47,10 +45,18 @@ class _SwipeFeedPageState extends State<SwipeFeedPage> {
                 iconData: Icons.home,
                 title: "Home",
                 onclick: () {
-                  
+                final FancyBottomNavigationState fstate =
+                    bottomNavigationKey.currentState;
+                    fstate.setPage(2);
             }),
-            TabData(iconData: Icons.search, title: "Search"),
+            TabData(
+                iconData: Icons.search,
+                title: "Search",
+                onclick: () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=> CategoriesPage()))
+            ),
           ],
+          initialSelection: 0,
+          key: bottomNavigationKey,
           onTabChangedListener: (position) {
             setState(() {
               currentPage = position;
@@ -58,6 +64,30 @@ class _SwipeFeedPageState extends State<SwipeFeedPage> {
           },
         ),
     );
+  }
+
+  _getPage(int page) {
+    switch (page) {
+      case 0:
+        return Column(
+          children: <Widget>[
+            showAlignmentCards
+                ? CardsSectionAlignment(context)
+                : CardsSectionDraggable(),
+            /*buttonsRow()*/
+          ],
+        );
+      case 1: return CategoriesPage();
+      default:
+        return Column(
+          children: <Widget>[
+            showAlignmentCards
+                ? CardsSectionAlignment(context)
+                : CardsSectionDraggable(),
+            /*buttonsRow()*/
+          ],
+        );
+    }
   }
 
 }
