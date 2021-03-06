08e0dacd46faf1a8acd9cb527a7a79ecfd08ce65
SirBob01 <kleonardo52@gmail.com>
2021-03-06 19:20:42 +1100

    Remove unnecessary stuff in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'swipe_feed_page.dart';
-import 'backend_calls.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
